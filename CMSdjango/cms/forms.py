a180434b57d40692002fd9252d05211f6d299b25
Rahul2325 <rahul230494@gmail.com>
2022-08-06 11:11:46 +0530

    Updted withh implementing bootstrap

--- CMSdjango/cms/forms.py
+++ CMSdjango/cms/forms.py
@@ -1,11 +1,19 @@
+from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 
 class SignUpForm(UserCreationForm):
+    password1=forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
     class Meta:
         model=User
         fields=['username', 'first_name', 'last_name', 'email']
         labels={'email': 'Email'}
+        widgets={'first_name':forms.TextInput(attrs={'class':'form-control'}),
+        'last_name':forms.TextInput(attrs={'class':'form-control'}),
+        'username':forms.TextInput(attrs={'class':'form-control'}),
+        'email':forms.EmailInput(attrs={'class':'form-control'})
+        }
 
 class EditUserProfileForm(UserChangeForm):
     password=None
@@ -13,3 +21,8 @@ class EditUserProfileForm(UserChangeForm):
         model=User
         fields=['username', 'first_name', 'last_name', 'email']
         labels={'email': 'Email'}
+        widgets={'first_name':forms.TextInput(attrs={'class':'form-control'}),
+        'last_name':forms.TextInput(attrs={'class':'form-control'}),
+        'username':forms.TextInput(attrs={'class':'form-control'}),
+        'email':forms.EmailInput(attrs={'class':'form-control'})
+        }
